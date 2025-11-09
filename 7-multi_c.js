8522d3a80125173ff02183668f72f058338e9280
Laanre Samaila James <creativesamjay@gmail.com>
2025-11-09 23:29:48 +0100

    Edited 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,7 @@
 const arg = process.argv[2];
 const num = parseInt(arg);
 
-if (isNaN(num) || num < 1) {
-  console.log("Missing number of occurrences");
-} else {
+if (num > 0) {
   for (let i = 0; i < num; i++) {
     console.log("C is fun");
   }
